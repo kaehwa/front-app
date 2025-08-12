2e9c93ca9a72531ecfe6094fad7dc8c26c96a3a4
Onseted <Onseted@gmail.com>
2025-08-12 10:44:35 +0900

    commit to temp

--- app/result.tsx
+++ app/result.tsx
@@ -1,13 +1,17 @@
 // app/result.tsx   → 경로: "/result"
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import LowBars from './lowBars'
 
 export default function Result() {
   const { ids } = useLocalSearchParams<{ ids?: string }>();
   return (
-    <View className="flex-1 items-center justify-center bg-white px-6">
-      <Text className="text-2xl font-bold">취향 결과</Text>
-      <Text className="mt-2 text-gray-600">선택: {ids ?? "없음"}</Text>
-    </View>
+    <SafeAreaView>
+      <View className="flex-1 items-center justify-center bg-white px-6">
+        <Text className="text-2xl font-bold">취향 결과</Text>
+        <Text className="mt-2 text-gray-600">선택: {ids ?? "없음"}</Text>
+      </View>
+      <LowBars></LowBars>
+    </SafeAreaView>
   );
 }
